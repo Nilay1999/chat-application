ce33aa8ddd8f330741411c2a46e83e9461abdeff
Nilay <58843582+Nilay1999@users.noreply.github.com>
2021-04-02 13:55:25 +0530

    bug fix

--- Backend/socket.js
+++ Backend/socket.js
@@ -1,63 +1,61 @@
-const User = require('./app/models/userSchema');
-const Notification = require('./app/models/notification');
+const User = require("./app/models/userSchema");
+const Notification = require("./app/models/notification");
 
-module.exports = function(io) {
-
-    io.on('connection', (socket) => {
-
-        //console.log('SOCKET ID ' + socket.id);
-        //io.to(socket.id).emit('userConnected', 'I just met you');
-        socket.on('userConnected', (id) => {
-            let promise = new Promise(function(resolve, result) {
-                setTimeout(() => resolve(
-                    Notification.findOne({ userId: id, 'msg.read': false })
-                ), 1000)
-            })
-            promise.then(
-                result => io.to(socket.id).emit('notify', result),
-                error => console.log(error)
-            );
-        })
-
-        socket.on('requestSend', (id) => {
-            let promise = new Promise(function(resolve, result) {
-                setTimeout(() => resolve(
-                    Notification.findOne({ userId: id, 'msg.read': false })
-                ), 1000)
-            })
-            promise.then(
-                result => socket.broadcast.emit('notify', result),
-                error => console.log(error)
-            );
-        })
-
-
-        socket.on('loadMsg', (id) => {
-            let promise = new Promise(function(resolve, result) {
-                setTimeout(() => resolve(
-                    Notification.findOne({ userId: id })
-                ), 1000)
-            })
-            promise.then(
-                result => io.to(socket.id).emit('receiveMsg', result),
-                error => console.log(error)
-            );
-        })
-
-        socket.on('requestMsg', (id) => {
-            let promise = new Promise(function(resolve, result) {
-                setTimeout(() => resolve(
-                    Notification.findOne({ userId: id })
-                ), 1000)
-            })
-            promise.then(
-                result => socket.broadcast.emit('receiveMsg', result),
-                error => console.log(error)
-            );
-        })
-
-        socket.on('refreshChat', (convId) => {
-            io.emit('loadChat')
-        })
+module.exports = function (io) {
+  io.on("connection", (socket) => {
+    //console.log('SOCKET ID ' + socket.id);
+    //io.to(socket.id).emit('userConnected', 'I just met you');
+    socket.on("userConnected", (id) => {
+      let promise = new Promise(function (resolve, result) {
+        setTimeout(
+          () =>
+            resolve(Notification.findOne({ userId: id, "msg.read": false })),
+          1000
+        );
+      });
+      promise.then(
+        (result) => io.to(socket.id).emit("notify", result),
+        (error) => console.log(error)
+      );
     });
+
+    socket.on("requestSend", (id) => {
+      let promise = new Promise(function (resolve, result) {
+        setTimeout(
+          () =>
+            resolve(Notification.findOne({ userId: id, "msg.read": false })),
+          1000
+        );
+      });
+      promise.then(
+        (result) => socket.broadcast.emit("notify", result),
+        (error) => console.log(error)
+      );
+    });
+
+    socket.on("loadMsg", (id) => {
+      let promise = new Promise(function (resolve, result) {
+        setTimeout(() => resolve(Notification.findOne({ userId: id })), 1000);
+      });
+      promise.then(
+        (result) => io.to(socket.id).emit("receiveMsg", result),
+        (error) => console.log(error)
+      );
+    });
+
+    socket.on("requestMsg", (id) => {
+      let promise = new Promise(function (resolve, result) {
+        setTimeout(() => resolve(Notification.findOne({ userId: id })), 1000);
+      });
+      promise.then(
+        (result) => socket.broadcast.emit("receiveMsg", result),
+        (error) => console.log(error)
+      );
+    });
+
+    socket.on("refreshChat", () => {
+      console.log("Refressh");
+      socket.broadcast.emit("loadChat");
+    });
+  });
 };
--- Frontend/assets/js/chatRoom/chatMessage.js
+++ Frontend/assets/js/chatRoom/chatMessage.js
@@ -1,123 +1,126 @@
 let textarea = document.querySelector("#text-chat");
 let messageArea = document.querySelector(".message__area");
 var socket = io("http://localhost:3000", { transport: ["websocket"] });
-let msgTo = document.querySelector('.brand');
+let msgTo = document.querySelector(".brand");
 let convId = sessionStorage.getItem("convId");
 let userId = localStorage.getItem("id");
-let userName = sessionStorage.getItem('userName');
+let userName = sessionStorage.getItem("userName");
 
-$("#logout").on("click", function() {
-    localStorage.removeItem("x-auth-token");
-    localStorage.removeItem("id");
-    localStorage.removeItem("email");
-    sessionStorage.removeItem("convId");
-    window.location = "login.html";
+$("#logout").on("click", function () {
+  localStorage.removeItem("x-auth-token");
+  localStorage.removeItem("id");
+  localStorage.removeItem("email");
+  sessionStorage.removeItem("convId");
+  window.location = "login.html";
 });
 
 window.onload = function msgLoad() {
-    msgTo.innerHTML = `<h1>${userName}</h1>`;
-    console.log(sessionStorage.getItem('userName'))
-    $.ajax({
-        url: `${url}/getConversation`,
-        method: "post",
-        data: {
-            convId: convId,
-        },
-        headers: { "x-auth-token": localStorage.getItem("x-auth-token") },
-        success: function(response) {
-            console.log(response);
-            response.forEach((i) => {
-                console.log(i.body);
-                if (i.author == userId) {
-                    appendMessage(i, "outgoing");
-                } else {
-                    appendMessage(i, "incoming");
-                }
-                scrollToBottom();
-            });
-        },
-        error: function() {
-            alert("Server Error");
-        },
-    });
+  msgTo.innerHTML = `<h1>${userName}</h1>`;
+  console.log(sessionStorage.getItem("userName"));
+  $.ajax({
+    url: `${url}/getConversation`,
+    method: "post",
+    data: {
+      convId: convId,
+    },
+    headers: { "x-auth-token": localStorage.getItem("x-auth-token") },
+    success: function (response) {
+      console.log(response);
+      response.forEach((i) => {
+        console.log(i.body);
+        if (i.author == userId) {
+          appendMessage(i, "outgoing");
+        } else {
+          appendMessage(i, "incoming");
+        }
+        scrollToBottom();
+      });
+    },
+    error: function () {
+      alert("Server Error");
+    },
+  });
 };
 
-textarea.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-        sendMessage(e.target.value);
+textarea.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    if (e.target.value == "") {
+      alert("please Enter msg");
+    } else {
+      sendMessage(e.target.value);
     }
+  }
 });
 
 function sendMessage(message) {
-    let msg = {
-        body: message.trim(),
-    };
+  let msg = {
+    body: message.trim(),
+  };
 
-    $.ajax({
-        url: `${url}/addMsg`,
-        method: "post",
-        data: {
-            convId: convId,
-            userId: userId,
-            msgBody: message,
-        },
-        headers: { "x-auth-token": localStorage.getItem("x-auth-token") },
-        success: function(response) {
-            console.log(message);
-        },
-        error: function() {
-            alert("Server Error");
-        },
-    });
+  $.ajax({
+    url: `${url}/addMsg`,
+    method: "post",
+    data: {
+      convId: convId,
+      userId: userId,
+      msgBody: message,
+    },
+    headers: { "x-auth-token": localStorage.getItem("x-auth-token") },
+    success: function (response) {
+      socket.emit("refreshChat");
+    },
+    error: function () {
+      alert("Server Error");
+    },
+  });
 
-    appendMessage(msg, "outgoing");
-    textarea.value = "";
-    scrollToBottom();
+  appendMessage(msg, "outgoing");
+  textarea.value = "";
 
-    socket.emit("refreshChat");
+  scrollToBottom();
 }
 
 function appendMessage(msg, type) {
-    let mainDiv = document.createElement("div");
-    let className = type;
-    mainDiv.classList.add(className, "message");
+  let mainDiv = document.createElement("div");
+  let className = type;
+  mainDiv.classList.add(className, "message");
 
-    let markup = `
+  let markup = `
         <h4><img src="../assets/images/clock.png" alt="clock">${moment(
           msg.createdAt
         ).format("h:mm")}</h4>
         <p>${msg.body}</p>
     `;
-    mainDiv.innerHTML = markup;
-    messageArea.appendChild(mainDiv);
+  mainDiv.innerHTML = markup;
+  messageArea.appendChild(mainDiv);
 }
 
-socket.on("loadChat", function() {
-    $.ajax({
-        url: `${url}/getConversation`,
-        method: "post",
-        data: {
-            convId: convId,
-        },
-        success: function(response) {
-            messageArea.innerHTML = "";
-            response.forEach((i) => {
-                console.log(i.body);
-                if (i.author == userId) {
-                    appendMessage(i, "outgoing");
-                    scrollToBottom();
-                } else {
-                    appendMessage(i, "incoming");
-                    scrollToBottom();
-                }
-            });
-        },
-        error: function() {
-            alert("Server Error");
-        },
-    });
+socket.on("loadChat", function () {
+  $.ajax({
+    url: `${url}/getConversation`,
+    method: "post",
+    data: {
+      convId: convId,
+    },
+    success: function (response) {
+      messageArea.innerHTML = "";
+      response.forEach((i) => {
+        console.log(i.body);
+        if (i.author == userId) {
+          appendMessage(i, "outgoing");
+          scrollToBottom();
+        } else {
+          appendMessage(i, "incoming");
+          scrollToBottom();
+        }
+      });
+    },
+    error: function () {
+      alert("Server Error");
+    },
+  });
 });
 
 function scrollToBottom() {
-    messageArea.scrollTop = messageArea.scrollHeight;
+  messageArea.scrollTop = messageArea.scrollHeight;
 }
