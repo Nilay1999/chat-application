25c8c0535f7dba6223cd24566526c88d131f4ab5
Nilay <58843582+Nilay1999@users.noreply.github.com>
2021-04-20 10:25:31 +0530

    html

--- Backend/app/controllers/addFriend.js
+++ Backend/app/controllers/addFriend.js
@@ -1,5 +1,5 @@
-const User = require('../models/userSchema');
-const Notification = require('../models/notification');
+const User = require("../models/userSchema");
+const Notification = require("../models/notification");
 
 exports.addFriend = (req, res) => {
     const senderId = req.body._id;
@@ -8,102 +8,88 @@ exports.addFriend = (req, res) => {
 
     let senderName;
     User.findById(senderId, (err, data) => {
-        senderName = data.userName
-    })
+        senderName = data.userName;
+    });
 
-    User.findOne({ _id: req.params.id, 'friend.sender': senderId }, (err, data) => {
-        if (err) {
-            console.log(err)
-        } else if (data) {
-            res.json({ msg: 'Friend Request Already Sent' })
-        } else {
-            User.findOne({ _id: req.params.id, 'friendList.friendId': senderId }, (err, data) => {
-                if (data) {
-                    res.json({ msg: 'Already In Friend List' })
-                } else {
-                    User.findByIdAndUpdate(req.params.id, {
-                        $push: {
-                            friend: [{
-                                sender: senderId,
-                                senderEmail: email
-                            }]
-                        }
-                    }, (err, data) => {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            res.json({ msg: 'Friend Request Sent' })
-                        }
-                    })
-
-
-                    Notification.findOne({ userId: receiver }, (err, data) => {
+    User.findOne(
+        { _id: req.params.id, "friend.sender": senderId },
+        (err, data) => {
+            if (err) {
+                console.log(err);
+            } else if (data) {
+                res.json({ msg: "Friend Request Already Sent" });
+            } else {
+                User.findOne(
+                    { _id: req.params.id, "friendList.friendId": senderId },
+                    (err, data) => {
                         if (data) {
-                            Notification.findOneAndUpdate({ userId: receiver }, {
-                                $push: {
-                                    msg: [{
-                                        body: `${senderName} sent you a friend request`
-                                    }]
-                                }
-                            }, (err, user) => {
-                                console.log('notification push')
-                            })
+                            res.json({ msg: "Already In Friend List" });
                         } else {
-                            const newNotification = new Notification({
-                                userId: receiver,
-                                msg: [{
-                                    body: `${senderName} sent you a friend request`
-                                }]
-                            })
-                            newNotification.save((err, data) => {
-                                if (err)
-                                    throw err;
-                            })
+                            User.findByIdAndUpdate(
+                                req.params.id,
+                                {
+                                    $push: {
+                                        friend: [
+                                            {
+                                                sender: senderId,
+                                                senderEmail: email,
+                                            },
+                                        ],
+                                    },
+                                },
+                                (err, data) => {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        res.json({
+                                            msg: "Friend Request Sent",
+                                        });
+                                    }
+                                }
+                            );
+
+                            Notification.findOne(
+                                { userId: receiver },
+                                (err, data) => {
+                                    if (data) {
+                                        Notification.findOneAndUpdate(
+                                            { userId: receiver },
+                                            {
+                                                $push: {
+                                                    msg: [
+                                                        {
+                                                            body: `${senderName} sent you a friend request`,
+                                                        },
+                                                    ],
+                                                },
+                                            },
+                                            (err, user) => {
+                                                console.log(
+                                                    "notification push"
+                                                );
+                                            }
+                                        );
+                                    } else {
+                                        const newNotification = new Notification(
+                                            {
+                                                userId: receiver,
+                                                msg: [
+                                                    {
+                                                        body: `${senderName} sent you a friend request`,
+                                                    },
+                                                ],
+                                            }
+                                        );
+                                        newNotification.save((err, data) => {
+                                            if (err) throw err;
+                                        });
+                                    }
+                                }
+                            );
                         }
-                    })
-                }
-            })
-
-            // User.findByIdAndUpdate(req.params.id, {
-            //     $push: {
-            //         friend: [{
-            //             sender: senderId,
-            //             senderEmail: email
-            //         }]
-            //     }
-            // }, (err, data) => {
-            //     if (err) {
-            //         console.log(err)
-            //     } else {
-            //         res.json({ msg: 'Friend Request Sent' })
-            //     }
-            // })
-
-
-            // Notification.findOne({ userId: receiver }, (err, data) => {
-            //     if (data) {
-            //         Notification.findOneAndUpdate({ userId: receiver }, {
-            //             $push: {
-            //                 msg: [{
-            //                     body: `${senderName} sent you a friend request`
-            //                 }]
-            //             }
-            //         }, (err, user) => {
-            //             console.log('notification push')
-            //         })
-            //     } else {
-            //         const newNotification = new Notification({
-            //             userId: receiver,
-            //             msg: [{
-            //                 body: `${senderName} sent you a friend request`
-            //             }]
-            //         })
-            //         newNotification.save((err, data) => {
-            //             if (err)
-            //                 throw err;
-            //         })
-            //     }
-            // })
+                    }
+                );
+            }
         }
-    })
-}
+    );
+};
