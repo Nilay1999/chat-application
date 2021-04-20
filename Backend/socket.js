ab8b962929592ae24cc4956eb707f00ad3343753
Nilay <58843582+Nilay1999@users.noreply.github.com>
2021-04-20 10:12:00 +0530

    formatting

--- Backend/socket.js
+++ Backend/socket.js
@@ -2,59 +2,73 @@ const User = require("./app/models/userSchema");
 const Notification = require("./app/models/notification");
 
 module.exports = function (io) {
-  io.on("connection", (socket) => {
-    //console.log('SOCKET ID ' + socket.id);
-    //io.to(socket.id).emit('userConnected', 'I just met you');
-    socket.on("userConnected", (id) => {
-      let promise = new Promise(function (resolve, result) {
-        setTimeout(
-          () =>
-            resolve(Notification.findOne({ userId: id, "msg.read": false })),
-          1000
-        );
-      });
-      promise.then(
-        (result) => io.to(socket.id).emit("notify", result),
-        (error) => console.log(error)
-      );
-    });
+    io.on("connection", (socket) => {
+        socket.on("userConnected", (id) => {
+            let promise = new Promise(function (resolve, result) {
+                setTimeout(
+                    () =>
+                        resolve(
+                            Notification.findOne({
+                                userId: id,
+                                "msg.read": false,
+                            })
+                        ),
+                    1000
+                );
+            });
+            promise.then(
+                (result) => io.to(socket.id).emit("notify", result),
+                (error) => console.log(error)
+            );
+        });
 
-    socket.on("requestSend", (id) => {
-      let promise = new Promise(function (resolve, result) {
-        setTimeout(
-          () =>
-            resolve(Notification.findOne({ userId: id, "msg.read": false })),
-          1000
-        );
-      });
-      promise.then(
-        (result) => socket.broadcast.emit("notify", result),
-        (error) => console.log(error)
-      );
-    });
+        socket.on("requestSend", (id) => {
+            let promise = new Promise(function (resolve, result) {
+                setTimeout(
+                    () =>
+                        resolve(
+                            Notification.findOne({
+                                userId: id,
+                                "msg.read": false,
+                            })
+                        ),
+                    1000
+                );
+            });
+            promise.then(
+                (result) => socket.broadcast.emit("notify", result),
+                (error) => console.log(error)
+            );
+        });
 
-    socket.on("loadMsg", (id) => {
-      let promise = new Promise(function (resolve, result) {
-        setTimeout(() => resolve(Notification.findOne({ userId: id })), 1000);
-      });
-      promise.then(
-        (result) => io.to(socket.id).emit("receiveMsg", result),
-        (error) => console.log(error)
-      );
-    });
+        socket.on("loadMsg", (id) => {
+            let promise = new Promise(function (resolve, result) {
+                setTimeout(
+                    () => resolve(Notification.findOne({ userId: id })),
+                    1000
+                );
+            });
+            promise.then(
+                (result) => io.to(socket.id).emit("receiveMsg", result),
+                (error) => console.log(error)
+            );
+        });
 
-    socket.on("requestMsg", (id) => {
-      let promise = new Promise(function (resolve, result) {
-        setTimeout(() => resolve(Notification.findOne({ userId: id })), 1000);
-      });
-      promise.then(
-        (result) => socket.broadcast.emit("receiveMsg", result),
-        (error) => console.log(error)
-      );
-    });
+        socket.on("requestMsg", (id) => {
+            let promise = new Promise(function (resolve, result) {
+                setTimeout(
+                    () => resolve(Notification.findOne({ userId: id })),
+                    1000
+                );
+            });
+            promise.then(
+                (result) => socket.broadcast.emit("receiveMsg", result),
+                (error) => console.log(error)
+            );
+        });
 
-    socket.on("refreshChat", () => {
-      socket.broadcast.emit("loadChat");
+        socket.on("refreshChat", () => {
+            socket.broadcast.emit("loadChat");
+        });
     });
-  });
 };
