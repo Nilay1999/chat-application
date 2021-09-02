0dd73c32f37f6cb8e263402c77b2552338e76d2c
Nilay <nilayparmar19@gmail.com>
2021-09-02 14:10:54 +0530

    error solve

---
+++ Backend/app/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/route.js');
+const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const mongoose = require('./connection'); // In-Use
+const http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: '*',
+    },
+});
+io = require('./socket')(io);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+require('dotenv').config();
+
+app.use('/app/uploads', express.static('/uploads'));
+app.use(cors({ origin: 'http://localhost:5500' }));
+
+app.use('/', routes);
+
+http.listen(PORT, () => {
+    console.log(`Server running at Port : ${PORT}`);
+});
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Node
- 
+
+# npm install @babel/core @babel/node @babel/preset-env --save-dev
+
+<!--
+touch .babelrc
+
+{
+  "presets": [
+    "@babel/preset-env"
+  ]
+}
+
+"start": "nodemon --exec babel-node src/index.js" -->
